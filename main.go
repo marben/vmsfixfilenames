456a6b7491ce945659b1a8cacd9eed68d1b41171
Martin Benes <martinbenesh@gmail.com>
2012-05-27 01:06:30 +0200

    rudimental parameters parsing

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
     "fmt"
+    "flag"
     )
 
 func main() {
     fmt.Println("This is a tool to fix vms ;256 style end of files")
+
+    var lowerCaseOpt *bool = flag.Bool("l", false, "Convert filenames to lowercase")
+    var upperCaseOpt *bool = flag.Bool("u", false, "Convert filenames to uppercase")
+
+    flag.Parse()
+    lowerCase := *lowerCaseOpt
+    upperCase := *upperCaseOpt
+
+    if lowerCase == true {
+        fmt.Println("LowerCase opt defined")
+    }
+    if upperCase == true {
+        fmt.Println("UpperCase opt defined")
+    }
+
 }
