a686d842cbd4690c283490faa48660f5d2a0d2a1
Martin Benes <martinbenesh@gmail.com>
2012-07-01 18:50:43 +0200

    fixed issue 2

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ import (
 	"strings"
 )
 
+var lowerCase = false
+var upperCase = false
+var force = false
+
 func usage() {
 	fmt.Println("This tool renames all the files in a directory that have the vms's file.ext;number pattern to a name without it..")
 	fmt.Println("Usage: vmsfixfilenames <dir>")
 	fmt.Println("Options: -l or -u to convert filenames to lower/upper case")
+	fmt.Println("\t -f - force rename even if the filename is not vms style")
 }
 
 var vmsRegexp = regexp.MustCompile("^(.*\\..*);(\\d+)$")
@@ -27,20 +32,34 @@ func vmsFilename(filename string) bool {
 }
 
 func vmsFixFilename(vmsFilename string) (string, error) {
+	var output string
 	matches := vmsRegexp.FindStringSubmatch(vmsFilename)
 	if matches == nil {
-		return "", errors.New("Filename is not vms style..")
+		if force {
+			output = vmsFilename
+		} else {
+			return "", errors.New("Filename is not vms style..")
+		}
+	} else {
+		output = matches[1]
 	}
-	return matches[1], nil
+
+	if upperCase {
+		output = strings.ToUpper(output)
+	}
+
+	if lowerCase {
+		output = strings.ToLower(output)
+	}
+
+	return output, nil
 }
 
 func main() {
-	var lowerCase bool
-	var upperCase bool
-
 	flag.Usage = usage
 	flag.BoolVar(&lowerCase, "l", false, "Convert filenames to lowercase")
 	flag.BoolVar(&upperCase, "u", false, "Convert filenames to uppercase")
+	flag.BoolVar(&force, "f", false, "Convert to lower/uppercase even if the file is not vms style")
 
 	flag.Parse()
 
@@ -71,20 +90,32 @@ func main() {
 	}
 
 	for _, file := range files {
-		if vmsFilename(file.Name()) {
+		if vmsFilename(file.Name()) || force {
 			newFilename, err := vmsFixFilename(file.Name())
 			if err != nil {
 				panic("panic!!")
 				os.Exit(1)
 			}
-			if upperCase {
-				newFilename = strings.ToUpper(newFilename)
-			}
-			if lowerCase {
-				newFilename = strings.ToLower(newFilename)
-			}
 
 			fmt.Printf("%s -> %s\n", file.Name(), newFilename)
+		}
+	}
+
+	fmt.Println("Do you want to continue? [Y/n]")
+	var reply string
+	fmt.Scanf("%s", &reply)
+	reply = strings.ToLower(reply)
+	if reply != "y" && reply != "yes" {
+		fmt.Println("Quitting.")
+		os.Exit(0)
+	}
+	for _, file := range files {
+		if vmsFilename(file.Name()) || force {
+			newFilename, err := vmsFixFilename(file.Name())
+			if err != nil {
+				panic("panic!!")
+				os.Exit(1)
+			}
 
 			err_rename := os.Rename(filepath.Join(dirname, file.Name()), filepath.Join(dirname, newFilename))
 			if err_rename != nil {
